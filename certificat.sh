0e885f561b17698b958db3bf80490ac80f068470
revolusys <99296583+revolusys@users.noreply.github.com>
2022-11-10 15:26:55 +0000

    Create certificat.sh

---
+++ certificat.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+{
+  wget -q --show-progress \
+    https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.6.3/linux/cfssl \
+    https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.6.3/linux/cfssljson
+  
+  sudo chmod +x cfssl cfssljson
+  sudo mv cfssl cfssljson /opt/bin/
+}
+
+cat > ca-config.json <<EOF
+{
+    "signing": {
+        "default": {
+            "expiry": "8760h"
+        },
+        "profiles": {
+            "etcd": {
+                "expiry": "8760h",
+                "usages": ["signing","key encipherment","server auth","client auth"]
+            }
+        }
+    }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "etcd cluster",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "GB",
+      "L": "England",
+      "O": "Kubernetes",
+      "OU": "ETCD-CA",
+      "ST": "Cambridge"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+
+
+#- Create TLS certificates (replace ETCD1_IP, ETCD2_IP ETCD3_IP by yours)
+
+{
+
+ETCD1_IP="192.168.1.161"
+ETCD2_IP="192.168.1.162"
+ETCD3_IP="192.168.1.163"
+
+cat > etcd-csr.json <<EOF
+{
+  "CN": "etcd",
+  "hosts": [
+    "localhost",
+    "127.0.0.1",
+    "${ETCD1_IP}",
+    "${ETCD2_IP}",
+    "${ETCD3_IP}"
+  ],
+  "key": {
+    "algo": "rsa",
+    "size": 4096
+  },
+  "names": [
+    {
+      "C": "GB",
+      "L": "England",
+      "O": "Kubernetes",
+      "OU": "etcd",
+      "ST": "Cambridge"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=etcd etcd-csr.json | cfssljson -bare etcd
